be225d5f2c8c5bd7a433e24a599c833867ee1e4d
Naram Alkoht <naramwork1@gmail.com.com>
2022-05-06 22:19:34 +0300

    test 1

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $tenant1 = App\Models\Tenant::create(['id' => 'naram']);
+    $tenant1->domains()->create(['domain' => 'naram.localhost']);
 });
